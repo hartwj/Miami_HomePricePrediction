d768f5818b3f9cd369ce2e886f56cf578db38e8b
zhoujuliana <56312807+zhoujuliana@users.noreply.github.com>
2020-10-15 00:46:03 -0400

    Date wrangling
    Converted saleYear to integer,
    created saleMonth integer variable

--- MUSA_Miami_Midterm.R
+++ MUSA_Miami_Midterm.R
@@ -1,7 +1,19 @@
+# Read neighborhoods and join to Miami - Julian
+# Read water and join to Miami - Juliana
+
+# --- Setup: Libraries ----
 
 library(tidyverse)
 library(sf)
+library(rmarkdown)
+library(kableExtra)
+library(viridis)
+library(tidycensus)
+library(osmdata)
+library(mapview)
+
 library(spdep)
+library(geosphere)
 library(caret)
 library(ckanr)
 library(FNN)
@@ -12,12 +24,12 @@ library(ggstance)
 library(jtools)     
 library(broom)
 # library(tufte)    #excluding for now..weird errors
-library(rmarkdown)
-library(kableExtra)
+
 library(readr)
+library(lubridate)
 
-
-# functions
+# --- Setup: Functions & Aesthetics ----
+## Setup functions
 mapTheme <- function(base_size = 12) {
   theme(
     text = element_text( color = "black"),
@@ -82,7 +94,7 @@ nn_function <- function(measureFrom,measureTo,k) {
     gather(points, point_distance, V1:ncol(.)) %>%
     arrange(as.numeric(thisPoint)) %>%
     group_by(thisPoint) %>%
-    summarize(pointDistance = mean(point_distance)) %>%
+    dplyr::summarize(pointDistance = mean(point_distance)) %>%
     arrange(as.numeric(thisPoint)) %>% 
     dplyr::select(-thisPoint) %>%
     pull()
@@ -90,23 +102,124 @@ nn_function <- function(measureFrom,measureTo,k) {
   return(output)  
 }
 
+# Load census API key
+census_api_key("dc04d127e79099d0fa300464507544280121fc3b", overwrite = TRUE)
 
+# --- Part 1. Data Wrangling ----
 
-## Reading in Data
+### Reading in Home Price Data & Base Map
+# Julian file path "C:/Users/12156/Documents/GitHub/Miami/studentsData.geojson"
+# JZhou file path "/Users/julianazhou/Documents/GitHub/Miami/studentsData.geojson"
 
-miamiHomes <- st_read("C:/Users/12156/Documents/GitHub/Miami/studentsData.geojson")
-miamiHomes.sf    <- miamiHomes %>% 
+miamiHomes <- st_read("/Users/julianazhou/Documents/GitHub/Miami/studentsData.geojson")
+miamiHomes.sf <- miamiHomes %>% 
   st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant") %>%
   st_transform('ESRI:102658')
 
-plot(miamiHomes.sf[,1])
+names(miamiHomes.sf)
+mapview(miamiHomes.sf[1,])
+glimpse(miamiHomes.sf)
 
-nhoods <- 
-  st_read("http://bostonopendata-boston.opendata.arcgis.com/datasets/3525b0ee6e6b427f9aab5d0a1d0a1a28_0.geojson") %>%
-  st_transform('ESRI:102286')
+# Read in base map
+miami.base <- 
+  st_read("https://opendata.arcgis.com/datasets/5ece0745e24b4617a49f2e098df8117f_0.geojson") %>%
+  st_transform('ESRI:102658') %>%
+  filter(NAME == "MIAMI BEACH" | NAME == "MIAMI") %>%
+  st_union()
+
+# Create border box around Miami base map to pull data from OSM
+xmin = st_bbox(miami.base)[[1]]
+ymin = st_bbox(miami.base)[[2]]
+xmax = st_bbox(miami.base)[[3]]  
+ymax = st_bbox(miami.base)[[4]]
+
+ggplot() +
+  geom_sf(data=miami.base, fill="black") +
+  geom_sf(data=st_as_sfc(st_bbox(miami.base)), colour="red", fill=NA) 
+
+#bars <- opq(bbox = c(xmin, ymin, xmax, ymax)) %>% #fix
+#  add_osm_feature(key = 'amenity', value = c("bar", "pub", "restaurant")) %>%
+#  osmdata_sf()
+
+### Joining shoreline distance to MiamiHomes.sf
+
+# Read in shoreline shapefile
+shoreline <- st_read('https://opendata.arcgis.com/datasets/58386199cc234518822e5f34f65eb713_0.geojson') %>% 
+  st_transform('ESRI:102658')
+
+# Find shoreline that intersects miami, and transform to points
+shoreline <- st_intersection(shoreline, miami.base)
+shoreline.point <- st_cast(shoreline,"POINT") 
+
+miamiHomes.sf <- miamiHomes.sf %>%
+  mutate(Shore1 = nn_function(st_coordinates(st_centroid(miamiHomes.sf)),
+                              st_coordinates(st_centroid(shoreline.point)),1)) %>%
+  mutate(Shore2 = nn_function(st_coordinates(st_centroid(miamiHomes.sf)),
+                              st_coordinates(st_centroid(shoreline.point)),2)) %>%
+  mutate(Shore3 = nn_function(st_coordinates(st_centroid(miamiHomes.sf)),
+                              st_coordinates(st_centroid(shoreline.point)),3)) %>%
+  mutate(Shore4 = nn_function(st_coordinates(st_centroid(miamiHomes.sf)),
+                              st_coordinates(st_centroid(shoreline.point)),4)) %>%
+  mutate(Shore5 = nn_function(st_coordinates(st_centroid(miamiHomes.sf)),
+                              st_coordinates(st_centroid(shoreline.point)),5))
+
+# Mapping distance to shore
+ggplot() + geom_sf(data=miami.base) + 
+  geom_sf(data=miamiHomes.sf, aes(colour=Shore1)) + 
+  scale_colour_viridis()
 
 
-# finding counts by group
+### Joining Census tract data to Miami.sf
+tracts <- get_acs(geography = "tract", variables = c(
+        "B25026_001E","B02001_002E","B19013_001E","B25058_001E","B06012_002E"), 
+        year=2017, state= 12, county= 086, geometry=T, output="wide") %>%
+  st_transform('ESRI:102658') %>%
+  rename(TotalPop = B25026_001E, 
+         Whites = B02001_002E,
+         MedHHInc = B19013_001E, 
+         MedRent = B25058_001E,
+         TotalPoverty = B06012_002E) %>%
+  dplyr::select(-NAME, -starts_with("B")) %>%
+  mutate(pctWhite = ifelse(TotalPop > 0, Whites / TotalPop,0),
+         pctPoverty = ifelse(TotalPop > 0, TotalPoverty / TotalPop, 0)) %>%
+  dplyr::select(-Whites, -TotalPoverty) 
+
+miamiHomes.sf <- st_join(miamiHomes.sf, tracts, join = st_within)
+
+# --- Part 2. Exploratory Analyses ----
+
+# Specify saleYear as integer, and create month feature
+miamiHomes.sf$saleYear <- as.integer(miamiHomes.sf$saleYear)
+miamiHomes.sf$saleMonth <- as.integer(month(mdy(miamiHomes.sf$saleDate)))
+
+glimpse(miamiHomes.sf) 
+
+### Corr matrix
+numericVars <- 
+  select_if(st_drop_geometry(miamiHomes.sf), is.numeric) %>% na.omit()
+
+ggcorrplot(
+  round(cor(numericVars), 1), 
+  p.mat = cor_pmat(numericVars),
+  colors = c("#25CB10", "white", "#FA7800"),
+  type="lower",
+  insig = "blank") +  
+  labs(title = "Correlation across numeric variables") 
+
+# --- Part 3. Feature Engineering ----
+
+# Specify saleYear as integer, and create month feature
+miamiHomes.sf$saleYear <- as.integer(miamiHomes.sf$saleYear)
+miamiHomes.sf$saleMonth <- as.integer(month(mdy(miamiHomes.sf$saleDate)))
+
+glimpse(miamiHomes.sf) 
+
+# --- Part 4. Feature Selection ----
+
+# --- Part 5. Model Estimation & Validation ----
+
+# --- Boston Lab ----
+####### finding counts by group
 plot1 <- group_by(bostonCrimes, OFFENSE_CODE_GROUP) %>%
   summarize(count = n()) %>%
   arrange(-count) %>% top_n(10)
@@ -250,6 +363,7 @@ reg1 <- lm(SalePrice ~ ., data = st_drop_geometry(boston.sf) %>%
                            R_BDRMS, R_FULL_BTH, R_HALF_BTH, 
                            R_KITCH, R_AC, R_FPLACE))
 summ(reg1)
+broom::glance(reg1)
 
 ## Plot of marginal response
 effect_plot(reg1, pred = R_BDRMS, interval = TRUE, plot.points = TRUE)
@@ -265,11 +379,272 @@ plot_summs(reg1, livingReg)
 ## Build a regression with LivingArea and crime_nn2
 ## report regression coefficient for LivingArea
 ## Is it different? Why?
-```
 
+## External model validation
 
+# Evaluating a model through its predictions on *new* data.
 
+# set random seed
+set.seed(31357)
 
+# get index for training sample
+inTrain <- caret::createDataPartition(
+  y = boston.sf$SalePrice, 
+  p = .60, list = FALSE)
+# split data into training and test
+boston.training <- boston.sf[inTrain,] 
+boston.test     <- boston.sf[-inTrain,]  
 
+# Regression  
+reg2 <- lm(SalePrice ~ ., data = st_drop_geometry(boston.training) %>% 
+             dplyr::select(SalePrice, LivingArea, 
+                           GROSS_AREA, R_TOTAL_RM, NUM_FLOORS,
+                           R_BDRMS, R_FULL_BTH, R_HALF_BTH, 
+                           R_KITCH, R_FPLACE))
 
+# Run this a number of times to see Adjusted R2
+summary(reg2)
 
+## predicting on new data
+reg2_predict <- predict(reg2, newdata = boston.test)
+
+## Measure generalizability?
+## Mean Square Error train and test
+rmse.train <- caret::MAE(predict(reg2), boston.training$SalePrice)
+rmse.test  <- caret::MAE(reg2_predict, boston.test$SalePrice)
+
+cat("Train MAE: ", as.integer(rmse.train), " \n","Test MAE: ", as.integer(rmse.test))
+
+preds.train <- data.frame(pred   = predict(reg2),
+                          actual = boston.training$SalePrice,
+                          source = "training data")
+preds.test  <- data.frame(pred   = reg2_predict,
+                          actual = boston.test$SalePrice,
+                          source = "testing data")
+preds <- rbind(preds.train, preds.test)
+
+ggplot(preds, aes(x = pred, y = actual, color = source)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "green") +
+  geom_abline(color = "orange") +
+  coord_equal() +
+  theme_bw() +
+  facet_wrap(~source, ncol = 2) +
+  labs(title = "Comparing predictions to actual values",
+       x = "Predicted Value",
+       y = "Actual Value") +
+  theme(
+    legend.position = "none"
+  )
+
+## Cross-validation
+
+# use caret package cross-validation method
+fitControl <- trainControl(method = "cv", 
+                           number = 10,
+                           # savePredictions differs from book
+                           savePredictions = TRUE)
+
+set.seed(717)
+# crimes.buffer feature added
+# for k-folds CV
+reg.cv <- 
+  train(SalePrice ~ ., data = st_drop_geometry(boston.sf) %>% 
+          dplyr::select(SalePrice, LivingArea,  
+                        GROSS_AREA, R_TOTAL_RM, NUM_FLOORS,
+                        R_BDRMS, R_FULL_BTH, R_HALF_BTH, 
+                        R_KITCH, R_FPLACE, crimes.Buffer), 
+        method = "lm", 
+        trControl = fitControl, 
+        na.action = na.pass)
+
+reg.cv
+
+reg.cv$resample
+
+reg.cv$resample %>% 
+  pivot_longer(-Resample) %>% 
+  mutate(name = as.factor(name)) %>% 
+  ggplot(., aes(x = name, y = value, color = name)) +
+  geom_jitter(width = 0.1) +
+  facet_wrap(~name, ncol = 3, scales = "free") +
+  theme_bw() +
+  theme(
+    legend.position = "none"
+  )
+
+# How do we interpret the spread of values between the folds?
+# extract predictions from CV object
+cv_preds <- reg.cv$pred
+# compare number of observations between data sets
+nrow(boston.sf)
+nrow(cv_preds)
+
+## Create dataset with "out of fold" predictions and original data
+map_preds <- boston.sf %>% 
+  rowid_to_column(var = "rowIndex") %>% 
+  left_join(cv_preds, by = "rowIndex") %>% 
+  mutate(SalePrice.AbsError = abs(pred - SalePrice)) %>% 
+  cbind(st_coordinates(.))
+# weird CRS fix to boston.sf
+st_crs(map_preds) <- st_crs(nhoods)
+
+# plot errors on a map
+ggplot() +
+  geom_sf(data = nhoods, fill = "grey40") +
+  geom_sf(data = map_preds, aes(colour = q5(SalePrice.AbsError)),
+          show.legend = "point", size = 1) +
+  scale_colour_manual(values = palette5,
+                      labels=qBr(map_preds,"SalePrice.AbsError"),
+                      name="Quintile\nBreaks") +
+  labs(title="Absolute sale price errors on the OOF set",
+       subtitle = "OOF = 'Out Of Fold'") +
+  mapTheme()
+
+## Spatial Correlation of Errors
+
+# Going back to single random data split to align with book
+
+inTrain <- createDataPartition(
+  y = paste(boston.sf$Name, boston.sf$NUM_FLOORS.cat, 
+            boston.sf$Style, boston.sf$R_AC), 
+  p = .60, list = FALSE)
+boston.training <- boston.sf[inTrain,] 
+boston.test <- boston.sf[-inTrain,]  
+
+reg.training <- 
+  lm(SalePrice ~ ., data = as.data.frame(boston.training) %>% 
+       dplyr::select(SalePrice, LivingArea, Style, 
+                     GROSS_AREA, NUM_FLOORS.cat,
+                     R_BDRMS, R_FULL_BTH, R_HALF_BTH, 
+                     R_KITCH, R_AC, R_FPLACE, crimes.Buffer))
+
+boston.test <-
+  boston.test %>%
+  mutate(Regression = "Baseline Regression",
+         SalePrice.Predict = predict(reg.training, boston.test),
+         SalePrice.Error = SalePrice.Predict - SalePrice,
+         SalePrice.AbsError = abs(SalePrice.Predict - SalePrice),
+         SalePrice.APE = (abs(SalePrice.Predict - SalePrice)) / SalePrice.Predict)%>%
+  filter(SalePrice < 5000000) 
+
+k_nearest_neighbors = 5
+#prices
+coords <- st_coordinates(boston.sf) 
+# k nearest neighbors
+neighborList <- knn2nb(knearneigh(coords, k_nearest_neighbors))
+spatialWeights <- nb2listw(neighborList, style="W")
+boston.sf$lagPrice <- lag.listw(spatialWeights, boston.sf$SalePrice)
+
+#errors
+coords.test <-  st_coordinates(boston.test) 
+neighborList.test <- knn2nb(knearneigh(coords.test, k_nearest_neighbors))
+spatialWeights.test <- nb2listw(neighborList.test, style="W")
+boston.test$lagPriceError <- lag.listw(spatialWeights.test, boston.test$SalePrice.AbsError)
+
+ggplot(boston.sf, aes(x=lagPrice, y=SalePrice)) +
+  geom_point(colour = "#FA7800") +
+  geom_smooth(method = "lm", se = FALSE, colour = "#25CB10") +
+  labs(title = "Price as a function of the spatial lag of price",
+       caption = "Public Policy Analytics, Figure 6.6",
+       x = "Spatial lag of price (Mean price of 5 nearest neighbors)",
+       y = "Sale Price") +
+  plotTheme()
+
+ggplot(boston.test, aes(x=lagPriceError, y=SalePrice)) +
+  geom_point(colour = "#FA7800") +
+  geom_smooth(method = "lm", se = FALSE, colour = "#25CB10") +
+  labs(title = "Error as a function of the spatial lag of price",
+       caption = "",
+       x = "Spatial lag of errors (Mean error of 5 nearest neighbors)",
+       y = "Sale Price") +
+  plotTheme()
+
+## Moran's I - A measure of spatial correlation
+
+moranTest <- moran.mc(boston.test$SalePrice.AbsError, 
+                      spatialWeights.test, nsim = 999)
+
+ggplot(as.data.frame(moranTest$res[c(1:999)]), aes(moranTest$res[c(1:999)])) +
+  geom_histogram(binwidth = 0.01) +
+  geom_vline(aes(xintercept = moranTest$statistic), colour = "#FA7800",size=1) +
+  scale_x_continuous(limits = c(-1, 1)) +
+  labs(title="Observed and permuted Moran's I",
+       subtitle= "Observed Moran's I in orange",
+       x="Moran's I",
+       y="Count",
+       caption="Public Policy Analytics, Figure 6.8") +
+  plotTheme()
+
+## Errors by group
+
+nhood_sum <- boston.test %>% 
+  group_by(Name) %>%
+  summarize(meanPrice = mean(SalePrice, na.rm = T),
+            meanPrediction = mean(SalePrice.Predict, na.rm = T),
+            meanMAE = mean(SalePrice.AbsError, na.rm = T))
+
+nhood_sum %>% 
+  st_drop_geometry %>%
+  arrange(desc(meanMAE)) %>% 
+  kable() %>% kable_styling()
+
+## Back to CV predictions for the moment. In the split sample predictions we only get predictions for the nhoods in the test sample; the rest are NA. In the CV split, we get a prediction on each nhood. That makes for a more complete looking map.
+
+map_preds_sum <- map_preds %>% 
+  group_by(Name) %>% 
+  summarise(meanMAE = mean(SalePrice.AbsError))
+
+ggplot() +
+  geom_sf(data = nhoods %>% 
+            left_join(st_drop_geometry(map_preds_sum), by = "Name"),
+          aes(fill = q5(meanMAE))) +
+  scale_fill_manual(values = palette5,
+                    labels=qBr(nhood_sum,"meanMAE"),
+                    name="Quintile\nBreaks") +
+  mapTheme() +
+  labs(title="Absolute sale price errors on the OOF set by Neighborhood")
+
+## Neighborhood Fixed Effect
+reg.nhood <- lm(SalePrice ~ ., data = as.data.frame(boston.training) %>% 
+                  dplyr::select(Name, SalePrice, LivingArea, 
+                                Style, GROSS_AREA, NUM_FLOORS.cat,
+                                R_BDRMS, R_FULL_BTH, R_HALF_BTH, 
+                                R_KITCH, R_AC, R_FPLACE,crimes.Buffer))
+
+boston.test.nhood <-
+  boston.test %>%
+  mutate(Regression = "Neighborhood Effects",
+         SalePrice.Predict = predict(reg.nhood, boston.test),
+         SalePrice.Error = SalePrice - SalePrice.Predict,
+         SalePrice.AbsError = abs(SalePrice - SalePrice.Predict),
+         SalePrice.APE = (abs(SalePrice - SalePrice.Predict)) / SalePrice) %>%
+  filter(SalePrice < 5000000)
+
+bothRegressions <- 
+  rbind(
+    dplyr::select(boston.test, starts_with("SalePrice"), Regression, Name) %>%
+      mutate(lagPriceError = lag.listw(spatialWeights.test, SalePrice.Error)),
+    dplyr::select(boston.test.nhood, starts_with("SalePrice"), Regression, Name) %>%
+      mutate(lagPriceError = lag.listw(spatialWeights.test, SalePrice.Error)))    
+
+st_drop_geometry(bothRegressions) %>%
+  gather(Variable, Value, -Regression, -Name) %>%
+  filter(Variable == "SalePrice.AbsError" | Variable == "SalePrice.APE") %>%
+  group_by(Regression, Variable) %>%
+  summarize(meanValue = mean(Value, na.rm = T)) %>%
+  spread(Variable, meanValue) %>%
+  kable() %>%
+  kable_styling("striped", full_width = F) %>%
+  row_spec(1, color = "black", background = "#25CB10") %>%
+  row_spec(2, color = "black", background = "#FA7800")
+
+### The key question to be able to answer from this is:
+
+# Which model better captures the spatial process of home sales prices and why?
+
+# model coefficents for each Neighborhood
+tidy(reg.nhood) %>% 
+  filter(str_detect(term, "Name")) %>% 
+  kable() %>% 
+  kable_styling()
